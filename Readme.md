0b94e2df7533dad628c75b5edbd0384cbf6ef7f1
Elias Junior <elijah.junnior@outlook.com>
2024-07-04 13:07:56 -0300

    Usando ícones da biblioteca phosphor

---
+++ Readme.md
@@ -0,0 +1,7 @@
+- Phosphor Icons
+
+  - Doc. : https://phosphoricons.com
+  - GitHub : https://github.com/duongdev/phosphor-react-native
+
+- React Native Svg
+  - Doc. : https://docs.expo.dev/versions/latest/sdk/svg/
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import { Container, Logo } from "./styles";
+import { CaretLeft } from "phosphor-react-native";
 
 import LogoImg from "@assets/logo.png";
 
 export function Header() {
   return (
     <Container>
+      <CaretLeft color="#cdcdcd" size={32} />
       <Logo source={LogoImg} />
     </Container>
   );
