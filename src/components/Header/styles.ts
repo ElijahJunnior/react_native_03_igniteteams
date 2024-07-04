ba1a4ff9e1ee695a4a8f6cbcdf477fd9506a3da6
Elias Junior <elijah.junnior@outlook.com>
2024-07-04 09:08:24 -0300

    criando component header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+`
+
+export const Logo = styled.Image`
+  height: 55px;
+  width: 46px;
+`
