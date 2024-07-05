9d23312b5e985b42decc78b54c70ff27430e17e7
Elias Junior <elijah.junnior@outlook.com>
2024-07-05 09:00:47 -0300

    adicionando lista de grupos a tela de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
-import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera do Fortnite",
+    "Galera do ChaDas22",
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do Fortnite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
